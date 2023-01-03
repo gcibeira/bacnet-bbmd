145309f1de8a06ecd1bf0574a7c92fa937fec7c8
G. Cibeira <gcibeira@gmail.com>
2023-01-03 16:49:22 +0100

    added some comments

--- example.js
+++ example.js
@@ -1,9 +1,11 @@
 const BACnetBBMD = require('./bacnet-bbmd');
+
 const bbmd = new BACnetBBMD({
-    broadcastAddress:'192.168.10.255',
-    port:47808,
-    bdt:[{address:'192.168.11.101:47809'}],
-    bbmdPort:47809
+    broadcastAddress:'192.168.10.255',      // Local subnet broadcast address
+    port:47808,                             // BACnet UDP port
+    bdt:[{address:'192.168.11.101:47809'}], // Broadcast Distribution Table
+    fdt:[],                                 // Foreign devices Distribution Table
+    bbmdPort:47809                          // Self UDP port to listen to other BBMDs and Foreign devices
 });
 
 console.log("server running...");
